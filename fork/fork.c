b8a0e761270c49531f8e545b220f709c20c12c51
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-20 20:00:00 +0200

    Add: Giving time for each of the proccesess to be completed. (using fflush to clear the buffer of the remainings in the buffer but in the 42Macs its not working).

--- fork/fork.c
+++ fork/fork.c
@@ -3,9 +3,20 @@
 int	main(void)
 {
 	int	id;
+	int	i;
+	int	n;
 
 	id = fork();
-	if (id != 0)
-		fork();
-	printf("Hello World\n");
+	if (id == 0)
+		n = 1;
+	else
+		n = 6;
+	i = n;
+	while (i < n + 5)
+	{
+		printf("%d", i);
+		fflush(stdout);
+		i++;
+	}
+	printf("\n");
 }
