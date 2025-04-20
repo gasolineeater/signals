c84665cfcf19c0b60f652525491091e3cf2465b9
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-20 20:13:45 +0200

    Add: wait function Now the program will wait and print the child process number first and then the main ones.

--- fork/fork.c
+++ fork/fork.c
@@ -5,13 +5,16 @@ int	main(void)
 	int	id;
 	int	i;
 	int	n;
-
+	
+	i = 0;
+	n = 
 	id = fork();
 	if (id == 0)
 		n = 1;
 	else
 		n = 6;
-	i = n;
+	if (id != 0)
+		wait(NULL); 
 	while (i < n + 5)
 	{
 		printf("%d", i);
