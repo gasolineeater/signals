d4e715627dcb50749e8a1a85eb930da87631df1e
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-21 12:53:41 +0200

    Add: printf that will print the pid of the parent function that got exec while wait function was waiting.

--- pid/pid.c
+++ pid/pid.c
@@ -3,10 +3,15 @@
 int	main(void)
 {
 	int	id;
+	int	res;
 
 	id = fork();
 	if (id == 0)
 		sleep(1);
 	printf("Current PID: %d parent: %d\n", getpid(), getppid());
-	wait(NULL);
+	res = wait(NULL);
+	if (res == -1)
+		printf("No children to wait for...\n");
+	else
+		printf("%d finished exec.\n", res);
 }
