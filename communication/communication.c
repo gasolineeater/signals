48f1f7b8889ac258875da7a0091013acb5097be2
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-21 13:27:19 +0200

    Add: a read function to read from pipes.

--- communication/communication.c
+++ communication/communication.c
@@ -15,4 +15,11 @@ int	main(int ac, char **av)
 		write(fd[1], &av[1], sizeof(int));
 		close(fd[1]);
 	}
+	else
+	{
+		close(fd[1]);
+		read(fd[0], &av[2], sizeof(int)); 
+		close(fd[0]);
+		printf("Got from child process : %s \n", av[2]);
+	}
 }
